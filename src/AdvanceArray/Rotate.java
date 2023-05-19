01a3f5ccd2394cfaa13545c065330e7627447e07
Kevin Dave <kevindave5735@gmail.com>
2023-05-19 12:02:09 +0530

    Rotate an array

--- src/AdvanceArray/Rotate.java
+++ src/AdvanceArray/Rotate.java
@@ -33,4 +33,16 @@ public class Rotate {
             arr[i] = temp[i];
         }
     }
+    static void LeftRotateByN(int[] arr, int n, int d) {
+        int[] temp = new int[d];
+        for (int i = 0; i < d; i++) {
+            temp[i] = arr[i];
+        }
+        for (int i = d; i < n; i++) {
+            arr[i-d] = arr[i];
+        }
+        for (int i = 0; i < d; i++) {
+            arr[n-d+i] = temp[i];
+        }
+    }
 }
